123641c7b10712487166da92bede4383776febae
Pixie35 <bxd363@student.bham.ac.uk>
2016-03-12 18:13:57 +0000

    added update to transaction

--- src/groovy/transactions/Transaction.groovy
+++ src/groovy/transactions/Transaction.groovy
@@ -1,10 +1,19 @@
 package groovy.transactions
 
-class Transaction {
+import nonGroovy.entitys.GameObject
+import nonGroovy.models.Renderable
+import nonGroovy.renderer.Colour
+
+class Transaction implements GameObject{
 	String time, description, tag, accountNumber, sortCode
 	TransactionEnums transactionType
 	double amount
-	
+
+	int x, y
+	int width, height
+	Colour colour
+	Renderable model;
+
 	Map transactionTypeMap = [
 		"CCD": TransactionEnums.CreditCardDeposit,
 		"CCW": TransactionEnums.CreditCardWithdrawal,
@@ -14,7 +23,7 @@ class Transaction {
 		"D/D": TransactionEnums.DirectDebit,
 		"BAC": TransactionEnums.BAC
 	]
-	
+
 	Transaction(String[] parts){
 		time = parts[1]
 		description = parts[2]
@@ -23,15 +32,17 @@ class Transaction {
 		tag = parts[5]
 		accountNumber = parts[6]
 	}
-	
+
 	double calculateAmountEffect(int hitAccuracy) {
 		if(amount >= 0) {
 			return amount * hitAccuracy / 100
 		} else if(amount < 0) {
 			return amount + ((HitAccuracy.PERFECT - hitAccuracy) * amount)/100
 		}
-		
 	}
-	
-	
+
+	@Override
+	public void update() {
+		x -=1;
+	}
 }
