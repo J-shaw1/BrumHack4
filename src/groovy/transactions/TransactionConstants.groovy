a0948e2441ac74bf1cc236cfdaef65aff5bc4f8c
Pixie35 <bxd363@student.bham.ac.uk>
2016-03-13 01:20:59 +0000

    I did a thing. Oh yeah, moving character to lanes

--- src/groovy/transactions/TransactionConstants.groovy
+++ src/groovy/transactions/TransactionConstants.groovy
@@ -5,8 +5,8 @@ public class TransactionConstants {
 	static final int STARTING_X = 1000
 	static final int Y_ONE = 600
 	static final int Y_TWO = 500
-	static final int Y_THREE = 400
-	static final int Y_FOUR = 300
+	static final int Y_THREE = 300
+	static final int Y_FOUR = 200
 	static final int WIDTH = 40
 	static final int HEIGHT = 40
 	static final int MOVE_SPEED = 4
--- src/nonGroovy/loop/DixonState.java
+++ src/nonGroovy/loop/DixonState.java
@@ -1,37 +1,51 @@
 package nonGroovy.loop;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import org.lwjgl.glfw.GLFW;
 
 import groovy.Character;
 import groovy.transactions.HealthBar;
+import groovy.transactions.MoveType;
+import groovy.transactions.Transaction;
+import groovy.transactions.TransactionConstants;
+import groovy.transactions.Transactions;
 import nonGroovy.entitys.GameObject;
 import nonGroovy.models.ColouredModel;
 import nonGroovy.renderer.BasicRenderer;
 import nonGroovy.renderer.Colour;
-import nonGroovy.window.Window;
 import nonGroovy.window.input.KeyInputCallback;
 
 class DixonState implements Loopable {
 
 	int xVel = 5;
 	int yVel = 5;
+	int oneTimeout;
+	int twoTimeout;
+	int threeTimeout;
+	int fourTimeout;
 
-	BasicRenderer renderer;
-	private ColouredModel testModel;
+	int timeoutReset = 30;
 
 	private ArrayList<GameObject> gameObjects;
 	private GameStateManager manager;
+	Transactions transactions;
+	long transactionInterval = (long) (1 * GameLoop.NANO_PER_SECOND);
+	long nextTransaction;
+
+	BasicRenderer renderer;
+	private ColouredModel testModel;
+	Character c;
 
 	public DixonState(GameStateManager manager) {
 		this.manager = manager;
 		renderer = new BasicRenderer();
 		gameObjects = new ArrayList<>();
 
-		Character c = new Character();
-		int width = 250;
-		int height = 540;
+		c = new Character();
+		int width = 80;
+		int height = 80;
 		c.setMoney(500.0);
 		c.setX(10 + width / 2);
 		c.setY(10 + height / 2);
@@ -59,29 +73,125 @@ class DixonState implements Loopable {
 		healthBarForeground.setHeight(height);
 		healthBarForeground.setGreen(true);
 
+		// demo lines
+		Character left = new Character();
+		left.setX(TransactionConstants.getPERFECT_HIT_X() - 100);
+		left.setY(500);
+		left.setWidth(1);
+		left.setHeight(1000);
+		left.setColour(new Colour(1f, 0f, 1f));
+
+		Character center = new Character();
+		center.setX(TransactionConstants.getPERFECT_HIT_X());
+		center.setY(500);
+		center.setWidth(1);
+		center.setHeight(1000);
+		center.setColour(new Colour(1f, 0f, 1f));
+
+		Character right = new Character();
+		right.setX(TransactionConstants.getPERFECT_HIT_X() + 100);
+		right.setY(500);
+		right.setWidth(1);
+		right.setHeight(1000);
+		right.setColour(new Colour(1f, 0f, 1f));
+
+		gameObjects.add(left);
+		gameObjects.add(center);
+		gameObjects.add(right);
+
 		gameObjects.add(healthBarBackground);
 		gameObjects.add(healthBarForeground);
 		gameObjects.add(c);
+
+		File file = new File("res/nationwideData/customer131.csv");
+		transactions = new Transactions(file);
+		nextTransaction = System.nanoTime() + transactionInterval;
 
 	}
 
 	@Override
 	public void input() {
-		if (KeyInputCallback.isKeyDown[GLFW.GLFW_KEY_SPACE]) {
-			System.out.println("Space");
+
+		if (KeyInputCallback.isKeyDown[GLFW.GLFW_KEY_KP_8] && oneTimeout < 0) {
+			oneTimeout = timeoutReset;
+			processAction(MoveType.one);
+			c.setY(TransactionConstants.getY_ONE());
+			System.out.println("KP8");
 		}
+
+		if (KeyInputCallback.isKeyDown[GLFW.GLFW_KEY_KP_5] && twoTimeout < 0) {
+			twoTimeout = timeoutReset;
+			processAction(MoveType.two);
+			c.setY(TransactionConstants.getY_TWO());
+			System.out.println("KP5");
+		}
+
+		if (KeyInputCallback.isKeyDown[GLFW.GLFW_KEY_KP_2] && threeTimeout < 0) {
+			threeTimeout = timeoutReset;
+			processAction(MoveType.three);
+			c.setY(TransactionConstants.getY_THREE());
+			System.out.println("KP2");
+		}
+
+		if (KeyInputCallback.isKeyDown[GLFW.GLFW_KEY_KP_0] && fourTimeout < 0) {
+			fourTimeout = timeoutReset;
+			processAction(MoveType.four);
+			c.setY(TransactionConstants.getY_FOUR());
+			System.out.println("KP0");
+		}
+
+		oneTimeout--;
+		twoTimeout--;
+		threeTimeout--;
+		fourTimeout--;
+
+	}
+
+	public void processAction(MoveType moveType) {
+
+		for (Transaction t : transactions)
+			if (t.getMoveTypes()[0] == moveType) {
+				if (t.getX() > TransactionConstants.getPERFECT_HIT_X() - TransactionConstants.getPERFECT_FLOAT() && t
+						.getX() < TransactionConstants.getPERFECT_HIT_X() + TransactionConstants.getPERFECT_FLOAT()) {
+					// if we have a hit
+					c.changeScore(
+							t.calculateAmountEffect(Math.abs(t.getX() - TransactionConstants.getPERFECT_HIT_X())));
+					t.setRemove(true);
+				}
+			}
 	}
 
 	@Override
 	public void update() {
+
+		if (System.nanoTime() > nextTransaction) {
+			transactions.gotoNext();
+			nextTransaction += transactionInterval;
+		}
+
+		// Clean up
+		for (int i = transactions.getPlace(); i >= 0; i--) {
+
+			if (transactions.get(i).getRemove() || (transactions.get(i).getX() < TransactionConstants.getPERFECT_HIT_X()
+					- TransactionConstants.getPERFECT_FLOAT() - 50)) {
+				transactions.remove(i);
+				transactions.backPlace();
+			} else {
+				transactions.get(i).update();
+			}
+		}
+
 		for (GameObject gameObject : gameObjects) {
 			gameObject.update();
 		}
+		// System.out.println("Score: " + score);
 	}
 
 	@Override
 	public void render() {
-
+		for (int i = 0; i <= transactions.getPlace(); i++) {
+			renderer.prepareEntity(transactions.get(i));
+		}
 		for (GameObject gameObject : gameObjects) {
 			renderer.prepareEntity(gameObject);
 		}
