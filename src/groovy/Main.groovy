65046894b62cb75e43057ab00beb8324f20225c2
Joe Shaw <Joe Shaw>
2016-03-13 04:13:14 +0000

    Commiting main before sarget major changes

--- src/groovy/Main.groovy
+++ src/groovy/Main.groovy
@@ -1,6 +1,25 @@
 package groovy
+
+import groovy.transactions.Transaction
+import groovy.transactions.Transactions
+
 class Main {
 	static void main(String[] args) {
-		println "Test"
+		File file = new File("res/nationwideData/customer688.csv")
+		
+		Transactions transactions = new Transactions(file)
+			
+		double total = 0
+		
+		Random random = new Random()
+		
+		transactions.each { Transaction transaction ->
+//			total += transaction.amount
+			println transaction.moveTypes
+			total += transaction.calculateAmountEffect(random.nextInt(101))
+		}
+		println transactions.size()
+		println total
 	}
+	
 }
