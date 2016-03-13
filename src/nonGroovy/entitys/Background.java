70f5c3247a53b7f5312f692bdbd9f3dbb28c8efe
sxm1041 <shaunm1995@hotmail.co.uk>
2016-03-13 01:43:15 +0000

    background++

---
+++ src/nonGroovy/entitys/Background.java
@@ -0,0 +1,57 @@
+package nonGroovy.entitys;
+
+import nonGroovy.models.ModelGenerator;
+import nonGroovy.models.TexturedModel;
+import nonGroovy.renderer.BasicRenderer;
+import nonGroovy.renderer.TextureLoader;
+
+public class Background {
+	
+	private double backgroundSpeed = -0.25;
+	private double parallaxSpeed = -0.5;
+	
+	private StaticObject backgroundA;
+	private StaticObject backgroundPA;
+	private StaticObject backgroundB;
+	private StaticObject backgroundPB;
+
+	public Background(double backgroundSpeed, double parallaxSpeed){
+		this.backgroundSpeed = backgroundSpeed;
+		this.parallaxSpeed = parallaxSpeed;
+		backgroundA = new StaticObject(540, 360, 1800, 720, new TexturedModel(ModelGenerator.square(), TextureLoader.loadTexture("background.png")));
+		backgroundPA = new StaticObject(540, 360, 1800, 720, new TexturedModel(ModelGenerator.square(), TextureLoader.loadTexture("backgroundParallax.png")));
+		backgroundB = new StaticObject(2340, 360, 1800, 720, new TexturedModel(ModelGenerator.square(), TextureLoader.loadTexture("background.png")));
+		backgroundPB = new StaticObject(2340, 360, 1800, 720, new TexturedModel(ModelGenerator.square(), TextureLoader.loadTexture("backgroundParallax.png")));
+	}
+	
+	public void update(){
+
+		backgroundA.increaseX(backgroundSpeed);
+		backgroundPA.increaseX(parallaxSpeed);
+		backgroundB.increaseX(backgroundSpeed);
+		backgroundPB.increaseX(parallaxSpeed);
+	
+	
+	if (backgroundA.getX() < -900) {
+		backgroundA.setX(backgroundB.getX()+1800);
+	}
+	if (backgroundPA.getX() < -900) {
+		backgroundPA.setX(backgroundPB.getX()+1800);
+	}
+	if (backgroundB.getX() < -900) {
+		backgroundB.setX(backgroundA.getX()+1800);
+	}
+	if (backgroundPB.getX() < -900) {
+		backgroundPB.setX(backgroundPA.getX()+1800);
+	}
+	}
+	
+	public void render(BasicRenderer renderer){
+		renderer.prepareEntity(backgroundA);
+		renderer.prepareEntity(backgroundB);
+		renderer.prepareEntity(backgroundPA);
+		renderer.prepareEntity(backgroundPB);
+	}
+	
+	
+}
--- src/nonGroovy/loop/ShaunState.java
+++ src/nonGroovy/loop/ShaunState.java
@@ -1,5 +1,6 @@
 package nonGroovy.loop;
 
+import nonGroovy.entitys.Background;
 import nonGroovy.entitys.GameObject;
 import nonGroovy.entitys.StaticObject;
 import nonGroovy.models.ModelGenerator;
@@ -13,17 +14,10 @@ class ShaunState implements Loopable {
 	
 	BasicRenderer renderer;
 
-	StaticObject backgroundA;
-	StaticObject backgroundB;
-	
-	StaticObject backgroundPA;
-	StaticObject backgroundPB;
+	Background background;
 	
 	public ShaunState() {
-		backgroundA = new StaticObject(540, 360, 1800, 720, new TexturedModel(ModelGenerator.square(), TextureLoader.loadTexture("background.png")));
-		backgroundPA = new StaticObject(540, 360, 1800, 720, new TexturedModel(ModelGenerator.square(), TextureLoader.loadTexture("backgroundParallax.png")));
-		backgroundB = new StaticObject(2340, 360, 1800, 720, new TexturedModel(ModelGenerator.square(), TextureLoader.loadTexture("background.png")));
-		backgroundPB = new StaticObject(2340, 360, 1800, 720, new TexturedModel(ModelGenerator.square(), TextureLoader.loadTexture("backgroundParallax.png")));
+		background = new Background(-0.5, -1);
 		
 		renderer = new BasicRenderer();
 		glClearColor(0,0,0,1);
@@ -36,36 +30,12 @@ class ShaunState implements Loopable {
 
 	@Override
 	public void update() {
-		for (int i = 0; i < 30; i++) {
-			backgroundA.increaseX(-0.25);
-			backgroundPA.increaseX(-0.5);
-			backgroundB.increaseX(-0.25);
-			backgroundPB.increaseX(-0.5);
-			
-		}
-		
-		System.out.println(backgroundPA.getX() + "   "  + backgroundPB.getX());
-		
-		if (backgroundA.getX() < -900) {
-			backgroundA.setX(backgroundB.getX()+1800);
-		}
-		if (backgroundPA.getX() < -900) {
-			backgroundPA.setX(backgroundPB.getX()+1800);
-		}
-		if (backgroundB.getX() < -900) {
-			backgroundB.setX(backgroundA.getX()+1800);
-		}
-		if (backgroundPB.getX() < -900) {
-			backgroundPB.setX(backgroundPA.getX()+1800);
-		}
+		background.update();
 	}
 
 	@Override
 	public void render() {
-		renderer.prepareEntity(backgroundA);
-		renderer.prepareEntity(backgroundB);
-		renderer.prepareEntity(backgroundPA);
-		renderer.prepareEntity(backgroundPB);
+		background.render(renderer);
 		renderer.render();
 	}
 
